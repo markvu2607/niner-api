deadb5ee9b798a367b6240f5486b00cd26980db0
Mark Vu <markvu.work@gmail.com>
2023-12-13 22:17:58 +0700

    feat: implement api ping

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,23 @@
-import express from "express"
+import dayjs from "dayjs"
+import express, { Request, Response } from "express"
 
+import { OkResponse } from "@/core/success.response.js"
+import catchAsync from "@/utils/catchAsync.js"
 import access from "./access/index.js"
 
 const router = express.Router()
+
+router.get(
+  "/ping",
+  catchAsync((req: Request, res: Response) => {
+    return new OkResponse({
+      message: "Welcome to Niner!",
+      metadata: {
+        timestamp: dayjs().unix(),
+      },
+    }).send(res)
+  }),
+)
 
 router.use("/auth", access)
 
